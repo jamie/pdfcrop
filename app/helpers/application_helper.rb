93ec46b4513eecfd7b9d21568ab2c28e9bd15824
Jamie Macey <jamie.git@tracefunc.com>
2024-06-02 18:48:42 -0700

    Add name to form fields for data submission

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def labelled_range(name, label, range, default)
+    <<~HTML.html_safe
+    <div>
+      <label for="#{name}" class="form-label">#{label}</label>
+      <input type="range" id="#{name}" name="#{name}" class="form-range" min="#{range.begin}" max="#{range.end}" value="#{default}" />
+    </div>
+    HTML
+  end
 end
